392a87cf50efb0c90d2a35b9ab5fda046c7e8462
Brainstorm release automation <releases@lifeisgoodlabs.com>
2026-07-20 01:48:58 +0200

    brainstorm 1.0.21

--- Casks/brainstorm.rb
+++ Casks/brainstorm.rb
@@ -1,8 +1,8 @@
 cask "brainstorm" do
-  version "1.0.19"
-  sha256 "9669dea25b3303310af35074f556765155769c7c234364b0bb46aef835119b21"
+  version "1.0.21"
+  sha256 "332b00e5e8b7dcb759942423063daa2d92318d9ba4f627652ca029847d9a534b"
 
-  url "https://github.com/eugenepyvovarov/brainstorm/releases/download/v1.0.19/Brainstorm-1.0.19.dmg",
+  url "https://github.com/eugenepyvovarov/brainstorm/releases/download/v1.0.21/Brainstorm-1.0.21.dmg",
       verified: "github.com/eugenepyvovarov/"
   name "Brainstorm"
   desc "Native macOS mind-map editor with a JSON-first CLI"
